8e2a3221c38e9b60c86b50f97272ce4885506c73
Muhammad Munaim Tahir <muhammad.tahir3@scholar.aku.edu>
2025-11-18 02:56:28 +0500

    Add VPS IP to allowed host configurations

--- backend/fmucontrolpanel/settings.py
+++ backend/fmucontrolpanel/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = 'django-insecure-_g@$f+liglbp=e!t-b=n6lv=_-cqx+io_&nsm2(xqxmf^n7q36
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    '172.235.33.181',
+]
 
 
 # Application definition
